4f27bc74e22d5bbce759f801a81448ed977f220d
El-bedevy <121668665+El-bedevy@users.noreply.github.com>
2023-08-16 08:14:43 +0000

    Deploying to gh-pages from @ El-bedevy/GlistEngine@aeb1fe7b2262ec068e953168b244320b4564850f 🚀

---
+++ navtreedata.js
@@ -0,0 +1,61 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "GlistEngine", "index.html", [
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classgBaseParticles.html#a22900021cfeb675625c63b18cd084307",
+"classgColorCurves.html",
+"classgGUICandleStickChart.html#a788db58bc9f8e922384571a593ac3c91",
+"classgGUIGraph.html#a2e66d7016075a36257344031922e9ed3",
+"classgGUINavigation.html#af0839fe470df6cf6316765d6d2d84ff0",
+"classgGUISlider.html#ae6e01ff744ebbd9c01060e69d6c6a0e4",
+"classgLight.html#a37651b6dfc093b75c8ff0d41b338ebb8",
+"classgModel.html#aef24db89c739f0534e468ef566b52e72",
+"classgRenderer.html#a717c416a501c98527a9d386cc77903d6",
+"classgTexture.html#a80e9f20fca3a996c92bcbf8d16cf5389",
+"gGUITable_8h_source.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ search/classes_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['cell_424',['Cell',['../structgGUIGrid_1_1Cell.html',1,'gGUIGrid']]]
+];
